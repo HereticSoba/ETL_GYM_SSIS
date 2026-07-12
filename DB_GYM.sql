4f3cb1c2d6cc7c611b740936be212f67d8d3ca83
Diego Farid Solorzano Villegas <PT72787033@idat.pe>
2026-07-12 13:56:30 -0500

    Correción de longitud en Gender y DifficultyLevel, varchar actualizado a 50. Con eso ya no aparece el simbolo de advertencia en Destino de OLE DB.

--- DB_GYM.sql
+++ DB_GYM.sql
@@ -10,7 +10,7 @@ CREATE TABLE AnalisisGym
 
 	--RF1 Perfil físico
 	Age DECIMAL(5,2),
-	Gender VARCHAR(20),
+	Gender VARCHAR(50),
 	WeigthKg DECIMAL(5,2),
 	BMI DECIMAL(5,2),
 
@@ -19,8 +19,8 @@ CREATE TABLE AnalisisGym
 	CaloriesBurned DECIMAL(7,2),
 	
 	--RF3 Intensidad Cardiaca
-	MaxBPM INT,
-	AvgBPM INT,
+	MaxBPM DECIMAL(6,2),
+	AvgBPM DECIMAL(6,2),
 
 	--RF4 Tipo de Entrenamiento
 	WorkoutType VARCHAR(50),
@@ -28,7 +28,7 @@ CREATE TABLE AnalisisGym
 	TargetMuscleGroup VARCHAR(100),
 
 	--RF5 Nivel de Dificultad
-	DifficultyLevel VARCHAR(30),
+	DifficultyLevel VARCHAR(50),
 
 	--RF6 Analisis Nutricional
 	DietType VARCHAR(50),
@@ -38,7 +38,7 @@ CREATE TABLE AnalisisGym
 	Fats DECIMAL(6,2),
 
 	--RF7 Filtrado
-	ExperienceLevel VARCHAR(50),
+	ExperienceLevel DECIMAL(3,2),
 
 	--RF8 Equipamiento
 	EquipmentNeeded VARCHAR(100),
@@ -47,38 +47,16 @@ CREATE TABLE AnalisisGym
 	BurnsCalories30Min DECIMAL(6,2),
 
 	--RF10 Alertas(DERIVADA SSIS)
-	AlertaSalud VARCHAR(50),
+	AlertaSalud NVARCHAR(50),
 );
 GO
 
 SELECT * FROM AnalisisGym;
 
 
-/*Columna Age tiene los datos en decimales, pero lo normal es que sean int,
-mientras lo dejamos en decimal entonces*/
-
-ALTER TABLE AnalisisGym
-ALTER COLUMN Age DECIMAL(5,2);
-GO
-
-/*Tambien en maxbpm, avgbpm (estaban int) y experiencelevel (estaba varchar 50)*/
-ALTER TABLE AnalisisGym
-ALTER COLUMN MaxBPM DECIMAL(6,2);
-
-ALTER TABLE AnalisisGym
-ALTER COLUMN AvgBPM DECIMAL(6,2);
-
-ALTER TABLE AnalisisGym
-ALTER COLUMN ExperienceLevel DECIMAL(3,2);
-GO
-
-/*Generaba problemas al ejecutar el SSIS, se cambió a NVARCHAR*/
-ALTER TABLE AnalisisGym
-ALTER COLUMN AlertaSalud NVARCHAR(50);
-
 sp_help AnalisisGym;
 
-SELECT COUNT(*)
+SELECT COUNT(*) AS Total_Registros
 FROM AnalisisGym;
 
 SELECT TOP 10 *
@@ -86,3 +64,4 @@ FROM AnalisisGym;
 
 /*Ejecutar antes de iniciar el SSIS, para que se duplique la data*/
 TRUNCATE TABLE AnalisisGym;
+
